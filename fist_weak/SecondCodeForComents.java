e02b125de96f71d74b22877f5ccabd4aa27acfa7
Javlon <abdufattorovjavlon@mail.ru>
2026-01-17 15:02:51 +0000

    add Coment code

---
+++ fist_weak/SecondCodeForComents.java
@@ -0,0 +1,17 @@
+public class SecondCodeForComents {
+    public static void main(String[] args) {
+        // Single line comment
+        /*
+        multi line comment
+         */
+    }
+
+}
+
+class Javlon{
+    /**
+     * @description -> This is Java doc comment
+     * @param a -> this is varible
+     */
+    int a;
+}
