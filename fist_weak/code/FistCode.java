bec6d0caac4d7502f559f5223298196c956878db
Javlon <abdufattorovjavlon@mail.ru>
2026-01-21 05:38:27 +0000

    Operators

--- fist_weak/code/FistCode.java
+++ fist_weak/code/FistCode.java
@@ -2,5 +2,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        int milliom = 1_000_000;
+        System.out.println(milliom);
     }
 }
