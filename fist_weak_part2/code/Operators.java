9ba03aab3138a1db49e371ae1e3ad5dffef4e8d9
Javlon <abdufattorovjavlon@mail.ru>
2026-01-21 06:11:39 +0000

    aeithmetic operators

---
+++ fist_weak_part2/code/Operators.java
@@ -0,0 +1,13 @@
+    package fist_weak_part2.code;
+    
+    public class Operators {
+        public static void main(String[] args) {
+            int a = 12;
+            int b = 2;
+            int resultOfAdition = a+b;// a,b -> operadns; + -> operators 
+            int resultOfSubstraction = a-b;
+            int c = 5;
+            int resultOfModuls = a%c;// При получение остатка от отрицательного числа в ответе появляется отрицательный остаток
+        }
+        
+    }
