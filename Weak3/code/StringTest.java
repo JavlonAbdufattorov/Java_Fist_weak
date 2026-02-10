c736ac6969d825a9511e940bf4d67a993b337787
Javlon <abdufattorovjavlon@mail.ru>
2026-02-10 16:12:08 +0000

    String2

--- Weak3/code/StringTest.java
+++ Weak3/code/StringTest.java
@@ -10,8 +10,11 @@ public class StringTest {
 
         String str1 = "QWER";
         String str2 = ".www";
-        String concat = str1 + str2;
+        String concat = str1 + str2;// есть метод конкат str1.concat(str2)
         System.out.println(concat);
+        // в джаве нету оверлодинга операторов то есть не можем поменять функционал +,- и так далее
+        // про стринг пул я слышал как она работает и что String    
+        
     }
     
 }
