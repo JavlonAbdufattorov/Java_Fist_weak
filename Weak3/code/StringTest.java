c715864647b2fb2147785b3de2a90d8dda0bd331
Javlon <abdufattorovjavlon@mail.ru>
2026-02-10 16:11:57 +0000

    String

---
+++ Weak3/code/StringTest.java
@@ -0,0 +1,17 @@
+package Weak3.code;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String word = "Hello word";
+        
+        String  newstr = word.substring(6);
+        System.out.println(newstr);
+        System.out.println(word.subSequence(6,8));// [6 ;8)
+
+        String str1 = "QWER";
+        String str2 = ".www";
+        String concat = str1 + str2;
+        System.out.println(concat);
+    }
+    
+}
